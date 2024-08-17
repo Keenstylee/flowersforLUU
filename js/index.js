3e006a130e05de23e5c9062a3d9cc6f9ee15deeb
Keen 3 <66754009+Keenstylee@users.noreply.github.com>
2024-08-17 14:17:21 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = 'I Have Something'.split('')
+const text = 'Estamos lejos pero sabes que siempre me las arreglo <3'.split('')
 for (let index = 0; index < text.length; index++) {
   if (text[index] !== ' ') {
     title.innerHTML += `<span>${text[index]}<span/>`
