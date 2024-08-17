67cc3f1b7acf94e06ab005312e35dd8172f0043b
Keen 3 <66754009+Keenstylee@users.noreply.github.com>
2024-08-17 18:00:14 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I MISS YOU').split('')
+    const titles = ('I MISS YOU BRUJA!').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
